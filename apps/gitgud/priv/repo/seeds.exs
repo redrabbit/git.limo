d1c2084b8763eea09932427de925546097785144
Mario Flach <m.flach@almightycouch.com>
2017-10-10 00:42:38 +0200

    Add user and repo to seeds.exs

--- apps/gitgud/priv/repo/seeds.exs
+++ apps/gitgud/priv/repo/seeds.exs
@@ -1,11 +1,7 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     GitGud.Repo.insert!(%GitGud.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias GitGud.Repo
+alias GitGud.User
+alias GitGud.Repository
+
+u = Repo.insert!(%User{username: "redrabbit", name: "Mario Flach", email: "m.flach@almightycouch.com"})
+r = Repo.insert!(%Repository{owner: u, path: "gitgud", name: "GitGud", description: "Git gud or git rekt!"})
+IO.inspect r
