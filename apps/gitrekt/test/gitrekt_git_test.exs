4b61626f505f9d758c5980e737b64b826b0cc34b
Mario Flach <m.flach@almightycouch.com>
2017-11-10 00:09:17 +0100

    Add GitRekt.GitTest
    See issue #3 for more details.

---
+++ apps/gitrekt/test/gitrekt_git_test.exs
@@ -0,0 +1,22 @@
+defmodule GitRekt.GitTest do
+  use ExUnit.Case
+
+  alias GitRekt.Git
+
+  setup do
+    {:ok, repo} = Git.repository_init("priv/test", false)
+    on_exit fn ->
+      File.rm_rf!(Git.repository_get_path(repo))
+    end
+
+    {:ok, %{repo: repo}}
+  end
+
+  test "ensures repository is valid", %{repo: repo} do
+    workdir = Path.expand("priv/test")
+    gitpath = Path.join(workdir, ".git")
+    refute Git.repository_bare?(repo)
+    assert Git.repository_get_workdir(repo) == "#{workdir}/"
+    assert Git.repository_get_path(repo) == "#{gitpath}/"
+  end
+end
