b1ed3df6c9866aef67e752d86505f61e5f0e8329
Mario Flach <m.flach@almightycouch.com>
2017-12-28 20:13:40 +0100

    Update GitGud.Web.Router

--- apps/gitgud_web/lib/gitgud_web/router.ex
+++ apps/gitgud_web/lib/gitgud_web/router.ex
@@ -15,34 +15,59 @@ defmodule GitGud.Web.Router do
     plug AuthenticationPlug
   end
 
+  # api + token authentication
   scope "/api", GitGud.Web do
     pipe_through :api
-    post "/token",                  AuthenticationController, :create, as: :user_token
+
+    # token generation
+    post "/token",                      AuthenticationController, :create, as: :user_token
+
     scope "/users/:user" do
       scope "/repos" do
-        resources "/",              RepositoryController, param: "repo", except: [:new, :edit]
+
+        # repositories
+        resources "/",                  RepositoryController, param: "repo", except: [:new, :edit]
+
         scope "/:repo" do
-          get "/branches",          RepositoryController, :branch_list
-          get "/branches/:branch",  RepositoryController, :branch
-          get "/tags",              RepositoryController, :tag_list
-          get "/tags/:tag",         RepositoryController, :tag
-          get "/revwalk/:spec",     RepositoryController, :revwalk
-          get "/commits/:spec",     RepositoryController, :commit
-          get "/tree/:spec/*path",  RepositoryController, :browse_tree
+
+          # branches
+          get    "/branches",           RepositoryController, :branch_list
+          post   "/branches",           RepositoryController, :create_branch
+          get    "/branches/:branch",   RepositoryController, :branch
+          put    "/branches/:branch",   RepositoryController, :update_branch
+          delete "/branches/:branch",   RepositoryController, :delete_branch
+
+          # tags
+          get    "/tags",               RepositoryController, :tag_list
+          post   "/tags",               RepositoryController, :create_tag
+          get    "/tags/:tag",          RepositoryController, :tag
+          put    "/tags/:tag",          RepositoryController, :update_tag
+          delete "/tags/:tag",          RepositoryController, :delete_tag
+
+          # commits
+          get    "/revwalk/:spec",      RepositoryController, :revwalk
+          get    "/commits/:spec",      RepositoryController, :commit
+
+          # trees
+          get    "/tree/:spec/*path",   RepositoryController, :browse_tree
+          get    "/blob/:spec/*path",   RepositoryController, :download_blob
         end
       end
     end
   end
 
+  # git smart-http + basic auth
   scope "/:user/:repo", GitGud.Web do
-    get "/info/refs",              GitBackendController, :info_refs
-    get "/HEAD",                   GitBackendController, :head
-    post "/git-upload-pack",       GitBackendController, :upload_pack
-    post "/git-receive-pack",      GitBackendController, :receive_pack
+    get "/info/refs",                   GitBackendController, :info_refs
+    get "/HEAD",                        GitBackendController, :head
+    post "/git-upload-pack",            GitBackendController, :upload_pack
+    post "/git-receive-pack",           GitBackendController, :receive_pack
   end
 
   scope "/", GitGud.Web do
     pipe_through :browser
-    get "/*page",                   PageController, :index
+
+    # single page application
+    get "/*page",                       PageController, :index
   end
 end
