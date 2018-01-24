b620faa9e0d96166064eb544b76faa0bd0f77263
Mario Flach <m.flach@almightycouch.com>
2018-01-25 00:54:21 +0100

    Add GitGud.Web.ReactComponents

--- apps/gitgud_web/assets/js/app.js
+++ apps/gitgud_web/assets/js/app.js
@@ -3,10 +3,29 @@ import "phoenix_html"
 import React from "react"
 import ReactDOM from "react-dom"
 
-const Hello = function(name) {
-  return (
-    <div>Hello, {name}</div>
-  )
+class BranchSelect extends React.Component {
+  render() {
+    return (
+      <select onChange={this.change} defaultValue={this.props.spec.oid}>
+        {this.props.branches.map((branch, i) =>
+          <option key={i} value={branch.oid}>{branch.shorthand}</option>
+        )}
+      </select>
+    )
+  }
+
+  change(event) {
+    console.log(event.target.options[event.target.selectedIndex].text)
+  }
 }
 
-ReactDOM.render(Hello("Will"), document.getElementById("app"));
+document.addEventListener("DOMContentLoaded", e => {
+  const elements = document.querySelectorAll("[data-react-class]")
+  Array.prototype.forEach.call(elements, e => {
+    const targetId = document.getElementById(e.dataset.reactTargetId)
+    const targetDiv = targetId ? targetId : e
+    const reactProps = e.dataset.reactProps ? e.dataset.reactProps : "{}"
+    const reactElement = React.createElement(eval(e.dataset.reactClass), JSON.parse(reactProps))
+    ReactDOM.render(reactElement, targetDiv)
+  })
+})
