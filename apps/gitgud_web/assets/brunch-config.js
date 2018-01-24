dfb0205fdc24edee5562a3a1ef9574fbab97d05a
Mario Flach <m.flach@almightycouch.com>
2018-01-24 22:47:38 +0100

    Add support for React.js

--- apps/gitgud_web/assets/brunch-config.js
+++ apps/gitgud_web/assets/brunch-config.js
@@ -3,21 +3,6 @@ exports.config = {
   files: {
     javascripts: {
       joinTo: "js/app.js"
-
-      // To use a separate vendor.js bundle, specify two files path
-      // http://brunch.io/docs/config#-files-
-      // joinTo: {
-      //   "js/app.js": /^js/,
-      //   "js/vendor.js": /^(?!js)/
-      // }
-      //
-      // To change the order of concatenation of files, explicitly mention here
-      // order: {
-      //   before: [
-      //     "vendor/js/jquery-2.1.1.js",
-      //     "vendor/js/bootstrap.min.js"
-      //   ]
-      // }
     },
     stylesheets: {
       joinTo: "css/app.css"
@@ -28,25 +13,17 @@ exports.config = {
   },
 
   conventions: {
-    // This option sets where we should place non-css and non-js assets in.
-    // By default, we set this to "/assets/static". Files in this directory
-    // will be copied to `paths.public`, which is "priv/static" by default.
     assets: /^(static)/
   },
 
-  // Phoenix paths configuration
   paths: {
-    // Dependencies and current project directories to watch
     watched: ["static", "css", "js", "vendor"],
-    // Where to compile files to
     public: "../priv/static"
   },
 
-  // Configure your plugins
   plugins: {
     babel: {
-      // Do not use ES6 compiler in vendor code
-      ignore: [/vendor/]
+      presets: ["latest", "react"]
     }
   },
 
--- apps/gitgud_web/assets/js/app.js
+++ apps/gitgud_web/assets/js/app.js
@@ -1,21 +1,12 @@
-// Brunch automatically concatenates all files in your
-// watched paths. Those paths can be configured at
-// config.paths.watched in "brunch-config.js".
-//
-// However, those files will only be executed if
-// explicitly imported. The only exception are files
-// in vendor, which are never wrapped in imports and
-// therefore are always executed.
-
-// Import dependencies
-//
-// If you no longer want to use a dependency, remember
-// to also remove its path from "config.paths.watched".
 import "phoenix_html"
 
-// Import local files
-//
-// Local files can be imported directly using relative
-// paths "./socket" or full ones "web/static/js/socket".
+import React from "react"
+import ReactDOM from "react-dom"
 
-// import socket from "./socket"
+const Hello = function(name) {
+  return (
+    <div>Hello, {name}</div>
+  )
+}
+
+ReactDOM.render(Hello("Will"), document.getElementById("app"));
