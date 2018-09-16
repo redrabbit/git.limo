d368adbf4494ee41fd2aa8bbed04f05d2527d874
Mario Flach <m.flach@almightycouch.com>
2018-09-17 00:14:52 +0200

    Optimize last commit using Ecto joins

--- apps/gitgud/lib/gitgud/user_query.ex
+++ apps/gitgud/lib/gitgud/user_query.ex
@@ -3,20 +3,18 @@ defmodule GitGud.UserQuery do
   Conveniences for `GitGud.User` related queries.
   """
 
-  alias GitGud.Authorization
   alias GitGud.DB
   alias GitGud.User
+
+  import Ecto.Query
 
   @doc """
   Returns a user for the given `id`.
   """
   @spec by_id(pos_integer, keyword) :: User.t | nil
   def by_id(id, opts \\ []) do
-    {{preloads, viewer}, opts} = extract_opts(opts)
-    User
-    |> DB.get(id, opts)
-    |> DB.preload(preloads)
-    |> filter_visible(viewer)
+    {params, opts} = extract_opts(opts)
+    DB.one(user_query(id, params), opts)
   end
 
   @doc """
@@ -24,11 +22,8 @@ defmodule GitGud.UserQuery do
   """
   @spec by_username(binary, keyword) :: User.t | nil
   def by_username(username, opts \\ []) do
-    {{preloads, viewer}, opts} = extract_opts(opts)
-    User
-    |> DB.get_by([username: username], opts)
-    |> DB.preload(preloads)
-    |> filter_visible(viewer)
+    {params, opts} = extract_opts(opts)
+    DB.one(user_query({:username, username}, params), opts)
   end
 
   @doc """
@@ -36,27 +31,53 @@ defmodule GitGud.UserQuery do
   """
   @spec by_email(binary, keyword) :: User.t | nil
   def by_email(email, opts \\ []) do
-    {{preloads, viewer}, opts} = extract_opts(opts)
-    User
-    |> DB.get_by([email: email], opts)
-    |> DB.preload(preloads)
-    |> filter_visible(viewer)
+    {params, opts} = extract_opts(opts)
+    DB.one(user_query({:email, email}, params), opts)
   end
 
   #
   # Helpers
   #
 
-  defp filter_visible(nil, _viewer), do: nil
-  defp filter_visible(%User{} = user, :granted), do: user
-  defp filter_visible(%User{repositories: []} = user, _viewer), do: user
-  defp filter_visible(%User{repositories: repos} = user, viewer) do
-    %{user|repositories: Authorization.filter(viewer, repos, :read)}
+  defp user_query(id) when is_integer(id) do
+    where(User, id: ^id)
+  end
+
+  defp user_query({_key, _val} = where) do
+    where(User, ^List.wrap(where))
+  end
+
+  defp user_query(match, {preloads, viewer}) do
+    exec_preload(user_query(match), preloads, viewer)
+  end
+
+  defp exec_preload(query, [], _viewer), do: query
+  defp exec_preload(query, [preload|tail], viewer) do
+    query
+    |> join_preload(preload, viewer)
+    |> exec_preload(tail, viewer)
+  end
+
+  defp join_preload(query, :repositories, nil) do
+    query
+    |> join(:left, [u], r in assoc(u, :repositories), r.public == true)
+    |> preload([u, r], [repositories: r])
+  end
+
+  defp join_preload(query, :repositories, viewer) do
+    query
+    |> join(:left, [u], m in "repositories_maintainers", m.user_id == ^viewer.id)
+    |> join(:left, [u, m], r in assoc(u, :repositories), r.public == true or r.owner_id == ^viewer.id or m.repo_id == r.id)
+    |> preload([u, m, r], [repositories: r])
+  end
+
+  defp join_preload(query, preload, _viewer) do
+    preload(query, ^preload)
   end
 
   defp extract_opts(opts) do
     {preloads, opts} = Keyword.pop(opts, :preload, [])
-    {viewer, opts} = Keyword.pop(opts, :viewer, :granted)
-    {{preloads, viewer}, opts}
+    {viewer, opts} = Keyword.pop(opts, :viewer)
+    {{List.wrap(preloads), viewer}, opts}
   end
 end
