d294a722bb3b46995a6b2dfc0e2dc23d96fe8fd8
Mario Flach <m.flach@almightycouch.com>
2018-09-23 01:06:51 +0200

    Add navigation menu to repo settings

--- apps/gitgud_web/lib/gitgud_web/views/helpers/navigation_helpers.ex
+++ apps/gitgud_web/lib/gitgud_web/views/helpers/navigation_helpers.ex
@@ -11,14 +11,12 @@ defmodule GitGud.Web.NavigationHelpers do
   @doc """
   Returns `true` if `conn` matches the given route `helper`; otherwhise return `false`.
   """
-  @spec current_route?(Plug.Conn.t, atom) :: boolean
-  def current_route?(conn, helper) do
+  @spec current_route?(Plug.Conn.t, atom, []) :: boolean
+  def current_route?(conn, helper, action \\ [])
+  def current_route?(conn, helper, []) do
     controller_module(conn) == helper_controller(helper)
   end
 
-  @doc """
-  Returns `true` if `conn` matches the given route `helper` and `actions`; otherwhise return `false`.
-  """
   @spec current_route?(Plug.Conn.t, atom, [only: [atom]]) :: boolean
   def current_route?(conn, helper, only: actions) when is_list(actions) do
     current_route?(conn, helper) && action_name(conn) in actions
@@ -30,30 +28,20 @@ defmodule GitGud.Web.NavigationHelpers do
   end
 
   @spec current_route?(Plug.Conn.t, atom, atom) :: boolean
-  def current_route?(conn, helper, action) do
+  def current_route?(conn, helper, action) when is_atom(action) do
     current_route?(conn, helper) && action_name(conn) == action
   end
 
   @doc """
-  Generates a `:li` navigation item for the given `helper`.
+  Generate a navigation `tag` for the given `helper` and `action`.
   """
-  @spec navigation_item(Plug.Conn.t, atom, keyword) :: binary
-  def navigation_item(conn, helper, [do: block] = _do) do
-    attrs = if current_route?(conn, helper),
-        do: [class: "is-active"],
-      else: []
-    content_tag(:li, block, attrs)
-  end
-
-  @doc """
-  Generates a `:li` navigation item for the given `helper` and `action`.
-  """
-  @spec navigation_item(Plug.Conn.t, atom, keyword | atom, keyword) :: binary
-  def navigation_item(conn, helper, action, [do: block] = _do) do
+  @spec navigation_item(Plug.Conn.t, atom, keyword | atom, atom, keyword, [do: term]) :: binary
+  def navigation_item(conn, helper, action \\ [], tag \\ :li, attrs \\ [], [do: block]) do
+    class = "is-active"
     attrs = if current_route?(conn, helper, action),
-        do: [class: "is-active"],
-      else: []
-    content_tag(:li, block, attrs)
+      do: Keyword.update(attrs, :class, class, &("#{&1} #{class}")),
+    else: attrs
+    content_tag(tag, block, attrs)
   end
 
   #
