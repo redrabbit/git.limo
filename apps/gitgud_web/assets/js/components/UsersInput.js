a3c063503f71671b87add7c680ae06e4bbb726ff
Mario Flach <m.flach@almightycouch.com>
2018-09-25 14:46:09 +0200

    Return GraphQL search results in <UsersInput />

--- apps/gitgud_web/assets/js/components/UsersInput.js
+++ apps/gitgud_web/assets/js/components/UsersInput.js
@@ -7,7 +7,7 @@ class UsersInput extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      users: props.initialUsers,
+      usernames: props.initialUsers,
       input: ""
     }
     this.dropdown = React.createRef()
@@ -17,7 +17,8 @@ class UsersInput extends React.Component {
     this.handleBlur = this.handleBlur.bind(this)
     this.handleInputChange = this.handleInputChange.bind(this)
     this.handleInputKeyDown = this.handleInputKeyDown.bind(this)
-    this.handleRemoveItem = this.handleRemoveItem.bind(this)
+    this.handleAddUser = this.handleAddUser.bind(this)
+    this.handleRemoveUser = this.handleRemoveUser.bind(this)
   }
 
   render() {
@@ -25,22 +26,52 @@ class UsersInput extends React.Component {
       <div className="users-input dropdown" ref={this.dropdown}>
         <div className="dropdown-trigger">
           <div className="input field is-grouped" ref={this.inputContainer} onFocus={this.handleFocus} onBlur={this.handleBlur}>
-            {this.state.users.map((user, i) =>
+            {this.state.usernames.map((user, i) =>
               <div className="control" key={i}>
                 <div className="tags has-addons">
                   <a className="tag">{user}</a>
-                  <a className="tag is-delete" onClick={this.handleRemoveItem(i)}></a>
+                  <a className="tag is-delete" onClick={this.handleRemoveUser(i)}></a>
                 </div>
               </div>
             )}
             <div className="control is-expanded">
               <input type="text" className="input is-static" value={this.state.input} onChange={this.handleInputChange} onKeyDown={this.handleInputKeyDown} />
             </div>
-            <input type="hidden" id={this.props.id} name={this.props.name} value={this.state.users.join(",")} />
+            <input type="hidden" id={this.props.id} name={this.props.name} value={this.state.usernames.join(",")} />
           </div>
         </div>
         <div className="dropdown-menu">
           <div className="dropdown-content">
+            {this.state.input.length &&
+              <QueryRenderer
+                environment={environment}
+                query={graphql`
+                  query UsersInputQuery($input: String!) {
+                          userSearch(input: $input) {
+                            username
+                            name
+                          }
+                  }
+                `}
+                variables={{
+                  input: this.state.input
+                }}
+                render={({error, props}) => {
+                  if(error) {
+                    return <div>{error.message}</div>
+                  } else if(props) {
+                    return (
+                      props.userSearch.filter(user =>
+                        !this.state.usernames.includes(user.username)
+                      ).map((user, i) =>
+                        <a key={i} className="dropdown-item" onClick={this.handleAddUser(user.username)}>{user.username} <span className="has-text-grey">{user.name}</span></a>
+                      )
+                    )
+                  }
+                  return <div></div>
+                }}
+              />
+            }
           </div>
         </div>
       </div>
@@ -66,26 +97,28 @@ class UsersInput extends React.Component {
 
   handleInputKeyDown(event) {
     if(this.inputSubmitKeys.includes(event.keyCode)) {
-      if(this.state.input.length) {
-        const {value} = event.target
-        this.dropdown.current.classList.remove("is-active")
-        this.setState(state => ({
-          users: [...state.users, value],
-          input: ""
-        }))
-      }
       event.preventDefault()
-    } else if(event.keyCode == 8 && this.state.users.length && !this.state.input.length) {
+    } else if(event.keyCode == 8 && this.state.usernames.length && !this.state.input.length) {
       this.setState(state => ({
-        users: state.users.slice(0, state.users.length - 1)
+        usernames: state.usernames.slice(0, state.usernames.length - 1)
       }))
     }
   }
 
-  handleRemoveItem(index) {
+  handleAddUser(username) {
+    return () => {
+      this.dropdown.current.classList.remove("is-active")
+      this.setState(state => ({
+        usernames: [...state.usernames, username],
+        input: ""
+      }))
+    }
+  }
+
+  handleRemoveUser(index) {
     return () => {
       this.setState(state => ({
-        users: state.users.filter((item, i) => i !== index)
+        usernames: state.usernames.filter((username, i) => i !== index)
       }))
     }
   }
