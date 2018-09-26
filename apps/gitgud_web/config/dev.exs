8dfefdd7c71905320b3e50cee5328f06e068bd6a
Mario Flach <m.flach@almightycouch.com>
2018-09-26 20:42:14 +0200

    Add relay-compiler to watchers

--- apps/gitgud_web/config/dev.exs
+++ apps/gitgud_web/config/dev.exs
@@ -11,7 +11,9 @@ config :gitgud_web, GitGud.Web.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: [node: ["node_modules/brunch/bin/brunch", "watch", "--stdin", cd: Path.expand("../assets", __DIR__)]],
+  watchers: [
+    npm: ["run", "watch", cd: Path.expand("../assets", __DIR__)],
+  ],
   live_reload: [
     patterns: [
       ~r{priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$},
