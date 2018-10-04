1132193e7b6c94df1a3994ac623d280b64d42773
Mario Flach <m.flach@almightycouch.com>
2018-10-04 12:20:43 +0200

    Add Javascript to close flash messages

--- apps/gitgud_web/assets/js/app.js
+++ apps/gitgud_web/assets/js/app.js
@@ -3,4 +3,10 @@ import factory from "./react-factory"
 import env from "./relay-environment"
 import socket from "./socket"
 
+[...document.getElementsByClassName("message")].forEach(flash => {
+  flash.querySelector("button.delete").addEventListener("click", event => {
+    flash.remove();
+  })
+})
+
 document.addEventListener("DOMContentLoaded", factory)
