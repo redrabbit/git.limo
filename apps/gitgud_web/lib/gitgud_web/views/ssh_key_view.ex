443e1e045ca6625e8a8107755cdd3fc60627a71f
Mario Flach <m.flach@almightycouch.com>
2018-10-16 13:38:53 +0200

    Remove function SSHKeyView.ssh_keyfingerprint

--- apps/gitgud_web/lib/gitgud_web/views/ssh_key_view.ex
+++ apps/gitgud_web/lib/gitgud_web/views/ssh_key_view.ex
@@ -2,16 +2,6 @@ defmodule GitGud.Web.SSHKeyView do
   @moduledoc false
   use GitGud.Web, :view
 
-  alias GitGud.SSHKey
-
-  @spec ssh_key_fingerprint(SSHKey.t) :: binary
-  def ssh_key_fingerprint(ssh_key) do
-    case :public_key.ssh_decode(ssh_key.data, :public_key) do
-      [{decoded_key, _attrs}] ->
-        to_string(:public_key.ssh_hostkey_fingerprint(decoded_key))
-    end
-  end
-
   @spec title(atom, map) :: binary
   def title(:index, _assigns), do: "SSH keys"
 end
