f84db0a45f209945ff1904604d95c839774f94fb
Mario Flach <m.flach@almightycouch.com>
2018-11-09 23:52:45 +0100

    Implement Phoenix.HTML.Safe for Git objects
    GitReference, GitTag and GitTreeEntry will display an icon in front of
    the object name. GitCommit displays the short commit sha.
    
    We do not wrap Git objects into a link. It's the job of the template to
    do so; allowing more flexibility in template code.

--- apps/gitgud_web/lib/gitgud/git_commit.ex
+++ apps/gitgud_web/lib/gitgud/git_commit.ex
@@ -1,5 +1,11 @@
+import Phoenix.HTML.Tag
+
 defimpl Phoenix.Param, for: GitGud.GitCommit do
   def to_param(commit), do: GitRekt.Git.oid_fmt(commit.oid)
 end
 
-
+defimpl Phoenix.HTML.Safe, for: GitGud.GitCommit do
+  def to_iodata(commit) do
+    Phoenix.HTML.Safe.to_iodata(content_tag(:span, GitRekt.Git.oid_fmt_short(commit.oid), class: "sha"))
+  end
+end
---
+++ apps/gitgud_web/lib/gitgud/git_tree_entry.ex
@@ -0,0 +1,20 @@
+import Phoenix.HTML.Tag
+import Phoenix.HTML, only: [raw: 1]
+
+defimpl Phoenix.Param, for: GitGud.GitTreeEntry do
+  def to_param(tree_entry), do: tree_entry.name
+end
+
+defimpl Phoenix.HTML.Safe, for: GitGud.GitTreeEntry do
+  def to_iodata(tree_entry) do
+    Phoenix.HTML.Safe.to_iodata(
+      cond do
+        tree_entry.type == :tree ->
+          [content_tag(:span, content_tag(:i, [], class: "fa fa-folder"), class: "icon"), raw(tree_entry.name)]
+        tree_entry.type == :blob ->
+          [content_tag(:span, content_tag(:i, [], class: "fa fa-file"), class: "icon"), raw(tree_entry.name)]
+      end
+    )
+  end
+end
+
