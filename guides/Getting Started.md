571dcc4269dd392f49e3e3eca4c032cd863c4f0d
Mario Flach <m.flach@almightycouch.com>
2018-12-03 16:27:45 +0100

    Update docs/
    * Add "Getting Started" guide.
    * Group modules by categories.

---
+++ guides/Getting Started.md
@@ -0,0 +1,3 @@
+## Getting Started
+
+This guide is an introduction to [GitGud](https://github.com/almightycouch/gitgud),
--- mix.exs
+++ mix.exs
@@ -1,12 +1,15 @@
 defmodule GitGud.Umbrella.Mixfile do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [apps_path: "apps",
-     version: "0.1.0",
+     version: @version,
      name: "Git Gud",
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     docs: docs()]
   end
 
   #
@@ -16,5 +19,97 @@ defmodule GitGud.Umbrella.Mixfile do
   defp deps do
     [{:distillery, "~> 2.0"},
      {:ex_doc, "~> 0.19", only: :dev, runtime: false}]
+  end
+
+  defp docs do
+    [
+#     main: "GitGud",
+      source_ref: "v#{@version}",
+      canonical: "https://git.limo",
+      source_url: "https://github.com/almightycouch/gitgud",
+      extras: [
+        "guides/Getting Started.md",
+      ],
+      groups_for_modules: [
+        "Database": [
+          GitGud.DB,
+          GitGud.DBQueryable
+        ],
+        "Authorization": [
+          GitGud.Authorization,
+          GitGud.AuthorizationPolicies
+        ],
+        "Schemas": [
+          GitGud.Auth,
+          GitGud.Auth.Provider,
+          GitGud.Email,
+          GitGud.Maintainer,
+          GitGud.Repo,
+          GitGud.RepoQuery,
+          GitGud.SSHKey,
+          GitGud.User,
+          GitGud.UserQuery
+        ],
+        "Deployment": [
+          GitGud.ReleaseTasks
+        ],
+        "Git helper structs": [
+          GitGud.GitBlob,
+          GitGud.GitCommit,
+          GitGud.GitDiff,
+          GitGud.GitReference,
+          GitGud.GitTag,
+          GitGud.GitTree,
+          GitGud.GitTreeEntry,
+        ],
+        "Git Transfer Protocols": [
+          GitGud.SSHServer,
+          GitGud.SmartHTTPBackend
+        ],
+        "OAuth2.0": [
+          GitGud.OAuth2.GitHub
+        ],
+        "GraphQL": [
+          GitGud.GraphQL.Resolvers,
+          GitGud.GraphQL.Schema,
+          GitGud.GraphQL.Types
+        ],
+        "Email Delivery": [
+          GitGud.Mailer
+        ],
+        "Web": [
+          GitGud.Web,
+          GitGud.Web.AuthenticationPlug,
+          GitGud.Web.CodebaseController,
+          GitGud.Web.DateTimeFormatter,
+          GitGud.Web.EmailController,
+          GitGud.Web.Endpoint,
+          GitGud.Web.ErrorHelpers,
+          GitGud.Web.ErrorView,
+          GitGud.Web.FallbackController,
+          GitGud.Web.Gettext,
+          GitGud.Web.Gravatar,
+          GitGud.Web.LandingPageController,
+          GitGud.Web.MaintainerController,
+          GitGud.Web.NavigationHelpers,
+          GitGud.Web.OAuth2Controller,
+          GitGud.Web.PaginationHelpers,
+          GitGud.Web.ReactComponents,
+          GitGud.Web.RepoController,
+          GitGud.Web.Router.Helpers,
+          GitGud.Web.SSHKeyController,
+          GitGud.Web.SessionController,
+          GitGud.Web.UserController,
+          GitGud.Web.UserSocket
+        ],
+        "Git low-level APIs": [
+          GitRekt.Git,
+          GitRekt.Packfile,
+          GitRekt.WireProtocol,
+          GitRekt.WireProtocol.ReceivePack,
+          GitRekt.WireProtocol.UploadPack
+        ],
+      ]
+    ]
   end
 end
