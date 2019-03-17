redrabbit/git.limo

commit 8dffafcdd9d4f5c30a21bcaf1b0022fe3359db5c

Resolve GitReference type upfront

apps/gitgud/lib/gitgud/git/git_reference.ex
@@ -9,12 +9,12 @@ defmodule GitGud.GitReference do
   alias GitGud.GitCommit
   alias GitGud.GitTag
 
-  @enforce_keys [:oid, :name, :prefix, :repo, :__git__]
-  defstruct [:oid, :name, :prefix, :repo, :__git__]
+  @enforce_keys [:oid, :name, :prefix, :repo, :type, :__git__]
+  defstruct [:oid, :name, :prefix, :repo, :type, :__git__]
 
   @type reference_type :: :branch | :tag
 
-  @type t :: %__MODULE__{oid: Git.oid, name: binary, prefix: binary, repo: Repo.t, __git__: Git.repo}
+  @type t :: %__MODULE__{oid: Git.oid, name: binary, prefix: binary, type: reference_type, repo: Repo.t, __git__: Git.repo}
 
   @doc """
   Returns the object pointed at by `reference`.
@@ -30,14 +30,6 @@ defmodule GitGud.GitReference do
         {:error, reason}
     end
   end
-
-  @doc """
-  Returns the type of the given `reference`.
-  """
-  @spec type(t) :: {:ok, reference_type} | {:error, term}
-  def type(%__MODULE__{prefix: "refs/heads/"} = _reference), do: {:ok, :branch}
-  def type(%__MODULE__{prefix: "refs/tags/"} = _reference), do: {:ok, :tag}
-  def type(%__MODULE__{} = _reference), do: {:error, :invalid_reference_type}
 
   #
   # Protocols

apps/gitgud/lib/gitgud/schemas/repo.ex
@@ -521,13 +521,16 @@ defmodule GitGud.Repo do
   defp resolve_reference({name, nil, :oid, oid}, {repo, handle}) do
     prefix = Path.dirname(name) <> "/"
     shorthand = Path.basename(name)
-    %GitReference{oid: oid, name: shorthand, prefix: prefix, repo: repo, __git__: handle}
+    %GitReference{oid: oid, name: shorthand, prefix: prefix, type: resolve_reference_type(prefix), repo: repo, __git__: handle}
   end
 
   defp resolve_reference({name, shorthand, :oid, oid}, {repo, handle}) do
     prefix = String.slice(name, 0, String.length(name) - String.length(shorthand))
-    %GitReference{oid: oid, name: shorthand, prefix: prefix, repo: repo, __git__: handle}
+    %GitReference{oid: oid, name: shorthand, prefix: prefix, type: resolve_reference_type(prefix), repo: repo, __git__: handle}
   end
+
+  defp resolve_reference_type("refs/heads/"), do: :branch
+  defp resolve_reference_type("refs/tags/"), do: :tag
 
   defp resolve_tag({name, shorthand, :oid, oid}, {repo, handle}) do
     case Git.reference_peel(handle, name, :tag) do

commit 6809ba766eb1b1275a0ad3285038c7322d936379

Fix bugs introduced by commit 8dffafc

apps/gitgud/lib/gitgud/git/git_reference.ex
@@ -9,8 +9,8 @@ defmodule GitGud.GitReference do
   alias GitGud.GitCommit
   alias GitGud.GitTag
 
-  @enforce_keys [:oid, :name, :prefix, :repo, :type, :__git__]
-  defstruct [:oid, :name, :prefix, :repo, :type, :__git__]
+  @enforce_keys [:oid, :name, :prefix, :type, :repo, :__git__]
+  defstruct [:oid, :name, :prefix, :type, :repo, :__git__]
 
   @type reference_type :: :branch | :tag
 

apps/gitgud_web/lib/gitgud/git_reference.ex
@@ -7,11 +7,9 @@ end
 defimpl Phoenix.HTML.Safe, for: GitGud.GitReference do
   def to_iodata(reference) do
     Phoenix.HTML.Safe.to_iodata(
-      case GitGud.GitReference.type(reference) do
-        {:ok, :branch} ->
-          [content_tag(:span, content_tag(:i, [], class: "fa fa-code-branch"), class: "icon"), content_tag(:span, reference.name)]
-        {:ok, :tag} ->
-          [content_tag(:span, content_tag(:i, [], class: "fa fa-tag"), class: "icon"), content_tag(:span, reference.name)]
+      case reference.type do
+        :branch -> [content_tag(:span, content_tag(:i, [], class: "fa fa-code-branch"), class: "icon"), content_tag(:span, reference.name)]
+        :tag -> [content_tag(:span, content_tag(:i, [], class: "fa fa-tag"), class: "icon"), content_tag(:span, reference.name)]
       end
     )
   end

apps/gitgud_web/lib/gitgud/graphql/resolvers.ex
@@ -230,14 +230,6 @@ defmodule GitGud.GraphQL.Resolvers do
   end
 
   @doc """
-  Resolves the type for the given Git `reference` object.
-  """
-  @spec git_reference_type(GitReference.t, %{}, Absinthe.Resolution.t) :: {:ok, GitReference.type} | {:error, term}
-  def git_reference_type(%GitReference{} = reference, %{} = _args, _info) do
-    GitReference.type(reference)
-  end
-
-  @doc """
   Resolves the commit history starting from the given Git `revision` object.
   """
   @spec git_history(map, Absinthe.Resolution.t) :: {:ok, Connection.t} | {:error, term}

apps/gitgud_web/lib/gitgud/graphql/types.ex
@@ -173,7 +173,7 @@ defmodule GitGud.GraphQL.Types do
     field :name, non_null(:string)
 
     @desc "The type of the reference."
-    field :type, non_null(:git_reference_type), resolve: &Resolvers.git_reference_type/3
+    field :type, non_null(:git_reference_type)
 
     @desc "The object the reference points to."
     field :target, non_null(:git_object), resolve: &Resolvers.git_reference_target/3

apps/gitgud_web/lib/gitgud_web/views/codebase_view.ex
@@ -118,12 +118,7 @@ defmodule GitGud.Web.CodebaseView do
   @spec revision_type(Repo.git_object) :: atom
   def revision_type(%GitCommit{} = _object), do: :commit
   def revision_type(%GitTag{} = _object), do: :tag
-  def revision_type(%GitReference{} = ref) do
-    case GitReference.type(ref) do
-      {:ok, type} -> type
-      {:error, _reason} -> nil
-    end
-  end
+  def revision_type(%GitReference{} = ref), do: ref.type
 
   @spec tree_entries(GitTree.t) :: [GitTreeEntry.t]
   def tree_entries(%GitTree{} = tree) do