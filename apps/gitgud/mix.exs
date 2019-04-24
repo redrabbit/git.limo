8c839aa46be34c3a40e228b504cb01bfc6ba3062
Mario Flach <m.flach@almightycouch.com>
2019-04-24 17:51:45 +0200

    Bump version to 0.2.4

--- apps/gitgud/mix.exs
+++ apps/gitgud/mix.exs
@@ -3,7 +3,7 @@ defmodule GitGud.Mixfile do
 
   def project do
     [app: :gitgud,
-     version: "0.2.3",
+     version: "0.2.4",
      build_path: "../../_build",
      config_path: "../../config/config.exs",
      deps_path: "../../deps",
