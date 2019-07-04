4f9fb254a14be5bf7ad9a995e46a5bed5426f23e
Mario Flach <m.flach@almightycouch.com>
2019-07-04 21:30:30 +0200

    Disable logs for :absinthe

--- apps/gitgud_web/config/dev.exs
+++ apps/gitgud_web/config/dev.exs
@@ -42,3 +42,5 @@ config :phoenix, :stacktrace_depth, 20
 
 # Initialize plugs at runtime for faster development compilation
 config :phoenix, :plug_init_mode, :runtime
+
+config :absinthe, :log, false
