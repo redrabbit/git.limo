4c7c954385e3b9a5dfa08ad4ccb04b33a3251fba
Mario Flach <m.flach@almightycouch.com>
2019-07-04 21:30:09 +0200

    Enhance GPG key frontend

--- apps/gitgud_web/lib/gitgud_web/views/gpg_key_view.ex
+++ apps/gitgud_web/lib/gitgud_web/views/gpg_key_view.ex
@@ -2,6 +2,14 @@ defmodule GitGud.Web.GPGKeyView do
   @moduledoc false
   use GitGud.Web, :view
 
+  def format_key_id(key) do
+    key
+    |> Base.encode16()
+    |> String.graphemes()
+    |> Enum.chunk_every(4)
+    |> Enum.join(":")
+  end
+
   @spec title(atom, map) :: binary
   def title(:index, _assigns), do: "GPG keys"
   def title(:new, _assigns), do: "Add a new GPG key"
