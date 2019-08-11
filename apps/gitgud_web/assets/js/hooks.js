0136802a69b394cbb403f70d8a0f567772d19c4c
Mario Flach <m.flach@almightycouch.com>
2019-08-11 22:00:34 +0200

    Fix syntax highlighting for diff-table
    We need to reset the state of the parser before each next hunk
    otherwhise the highlighting is messed up.
    
    See #48 for more details.

--- apps/gitgud_web/assets/js/hooks.js
+++ apps/gitgud_web/assets/js/hooks.js
@@ -23,12 +23,25 @@ export default () => {
   document.querySelectorAll("table.blob-table").forEach(table => {
     const lang = table.dataset.lang
     const langDetect = hljs.getLanguage(lang)
-    let state
-    table.querySelectorAll("td.code .code-inner:not(.nohighlight)").forEach(line => {
+    const highlight = (line) => {
       const result = langDetect ? hljs.highlight(lang, line.textContent, true, state) : hljs.highlightAuto(line.textContent)
       state = result.top
       line.innerHTML = result.value
-    })
+    }
+    let state
+    if(table.classList.contains("diff-table")) {
+      let row = table.querySelector("tr.hunk")
+      while(row) {
+        row = row.nextElementSibling
+        if(!row || row.classList.contains("hunk")) {
+          state = undefined
+          continue
+        }
+        highlight(row.querySelector("td.code .code-inner:not(.highlight)"))
+      }
+    } else {
+      table.querySelectorAll("td.code .code-inner:not(.nohighlight)").forEach(highlight)
+    }
   })
 
   document.querySelectorAll("[data-react-class]").forEach(e => {
