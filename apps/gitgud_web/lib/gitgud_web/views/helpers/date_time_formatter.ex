d7b138c2d0a6e149b1b33655a0afc98dfd843711
Mario Flach <m.flach@almightycouch.com>
2019-08-15 21:46:22 +0200

    Add tooltip for relative datetime_format/2 <time>

--- apps/gitgud_web/lib/gitgud_web/views/helpers/date_time_formatter.ex
+++ apps/gitgud_web/lib/gitgud_web/views/helpers/date_time_formatter.ex
@@ -3,14 +3,23 @@ defmodule GitGud.Web.DateTimeFormatter do
   Conveniences for formatting `DateTime`, `Date` and `Time` values.
   """
 
+  import Phoenix.HTML.Tag
+
   @doc """
   Formats a date/time value using the given `format` string.
   """
   @spec datetime_format(DateTime.t, binary) :: binary
-  def datetime_format(datetime, format) do
+  def datetime_format(%DateTime{} = datetime, format) do
+    datetime_str = DateTime.to_iso8601(datetime)
     if String.contains?(format, "{relative}"),
-      do: Timex.format!(datetime, "{relative}", :relative),
-    else: Timex.format!(datetime, format)
+      do: content_tag(:time, Timex.format!(datetime, "{relative}", :relative), datetime: datetime_str, data_tooltip: datetime_str, class: "tooltip"),
+    else: content_tag(:time, Timex.format!(datetime, format), datetime: datetime_str)
+  end
+
+  def datetime_format(%NaiveDateTime{} = datetime, format) do
+    datetime_str = NaiveDateTime.to_iso8601(datetime)
+    if String.contains?(format, "{relative}"),
+      do: content_tag(:time, Timex.format!(datetime, "{relative}", :relative), datetime: datetime_str, data_tooltip: datetime_str, class: "tooltip"),
+    else: content_tag(:time, Timex.format!(datetime, format), datetime: datetime_str)
   end
 end
-
