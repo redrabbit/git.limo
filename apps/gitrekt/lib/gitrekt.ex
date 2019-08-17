8bec8bc760be07b364fc010bd0fed92e58758046
Mario Flach <m.flach@almightycouch.com>
2019-08-18 00:26:37 +0200

    Implement Inspect protocol for Git structs

--- apps/gitrekt/lib/gitrekt.ex
+++ apps/gitrekt/lib/gitrekt.ex
@@ -9,6 +9,10 @@ defmodule GitRekt do
     """
     defstruct [:oid, :commit]
     @type t :: %__MODULE__{oid: Git.oid, commit: Git.commit}
+
+    defimpl Inspect do
+      def inspect(commit, _opts), do: "<GitCommit:#{Git.oid_fmt_short(commit.oid)}>"
+    end
   end
 
   defmodule GitRef do
@@ -17,6 +21,10 @@ defmodule GitRekt do
     """
     defstruct [:oid, :name, :prefix, :type]
     @type t :: %__MODULE__{oid: Git.oid, name: binary, prefix: binary, type: :branch | :tag}
+
+    defimpl Inspect do
+      def inspect(ref, _opts), do: "<GitRef:#{ref.prefix}#{ref.name}>"
+    end
   end
 
   defmodule GitTag do
@@ -25,6 +33,10 @@ defmodule GitRekt do
     """
     defstruct [:oid, :name, :tag]
     @type t :: %__MODULE__{oid: Git.oid, name: :binary, tag: Git.tag}
+
+    defimpl Inspect do
+      def inspect(tag, _opts), do: "<GitTag:#{tag.name}>"
+    end
   end
 
   defmodule GitBlob do
@@ -33,6 +45,10 @@ defmodule GitRekt do
     """
     defstruct [:oid, :blob]
     @type t :: %__MODULE__{oid: Git.oid, blob: Git.blob}
+
+    defimpl Inspect do
+      def inspect(blob, _opts), do: "<GitBlob:#{Git.oid_fmt_short(blob.oid)}>"
+    end
   end
 
   defmodule GitTree do
@@ -41,6 +57,10 @@ defmodule GitRekt do
     """
     defstruct [:oid, :tree]
     @type t :: %__MODULE__{oid: Git.oid, tree: Git.blob}
+
+    defimpl Inspect do
+      def inspect(tree, _opts), do: "<GitTree:#{Git.oid_fmt_short(tree.oid)}>"
+    end
   end
 
   defmodule GitTreeEntry do
@@ -49,6 +69,10 @@ defmodule GitRekt do
     """
     defstruct [:oid, :name, :mode, :type]
     @type t :: %__MODULE__{oid: Git.oid, name: binary, mode: integer, type: :blob | :tree}
+
+    defimpl Inspect do
+      def inspect(tree_entry, _opts), do: "<GitTreeEntry:#{tree_entry.name}>"
+    end
   end
 
   defmodule GitDiff do
@@ -57,5 +81,9 @@ defmodule GitRekt do
     """
     defstruct [:diff]
     @type t :: %__MODULE__{diff: Git.diff}
+
+    defimpl Inspect do
+      def inspect(diff, _opts), do: "<GitDiff:#{inspect diff.diff}>"
+    end
   end
 end
