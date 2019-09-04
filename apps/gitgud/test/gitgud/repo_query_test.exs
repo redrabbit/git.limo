a4b2485e74999701720868e1340f2598e9d7556c
Mario Flach <m.flach@almightycouch.com>
2019-09-05 01:34:43 +0200

    Remove :preload in UserQueryTest and RepoQueryTest

--- apps/gitgud/test/gitgud/repo_query_test.exs
+++ apps/gitgud/test/gitgud/repo_query_test.exs
@@ -11,35 +11,35 @@ defmodule GitGud.RepoQueryTest do
 
   test "gets single repository by id", %{repos: repos} do
     for repo <- repos do
-      assert repo.id == RepoQuery.by_id(repo.id, preload: :maintainers).id
+      assert repo.id == RepoQuery.by_id(repo.id).id
     end
   end
 
   test "gets multiple repositories by id", %{repos: repos} do
-    assert Enum.all?(RepoQuery.by_id(Enum.map(repos, &(&1.id)), preload: :maintainers), fn repo -> repo.id in Enum.map(repos, &(&1.id)) end)
+    assert Enum.all?(RepoQuery.by_id(Enum.map(repos, &(&1.id))), fn repo -> repo.id in Enum.map(repos, &(&1.id)) end)
   end
 
   test "gets single user repository", %{repos: repos} do
     for repo <- repos do
-      assert repo.id == RepoQuery.user_repo(repo.owner.id, repo.name, preload: :maintainers).id
-      assert repo.id == RepoQuery.user_repo(repo.owner.login, repo.name, preload: :maintainers).id
-      assert repo.id == RepoQuery.user_repo(repo.owner, repo.name, preload: :maintainers).id
+      assert repo.id == RepoQuery.user_repo(repo.owner.id, repo.name).id
+      assert repo.id == RepoQuery.user_repo(repo.owner.login, repo.name).id
+      assert repo.id == RepoQuery.user_repo(repo.owner, repo.name).id
     end
   end
 
   test "gets multiple repositories from single user", %{repos: repos} do
     for {user, repos} <- Enum.group_by(repos, &(&1.owner)) do
-      assert Enum.all?(RepoQuery.user_repos(user.id, preload: :maintainers), fn repo -> repo.id in Enum.map(repos, &(&1.id)) end)
-      assert Enum.all?(RepoQuery.user_repos(user.login, preload: :maintainers), fn repo -> repo.id in Enum.map(repos, &(&1.id)) end)
-      assert Enum.all?(RepoQuery.user_repos(user, preload: :maintainers), fn repo -> repo.id in Enum.map(repos, &(&1.id)) end)
+      assert Enum.all?(RepoQuery.user_repos(user.id), fn repo -> repo.id in Enum.map(repos, &(&1.id)) end)
+      assert Enum.all?(RepoQuery.user_repos(user.login), fn repo -> repo.id in Enum.map(repos, &(&1.id)) end)
+      assert Enum.all?(RepoQuery.user_repos(user), fn repo -> repo.id in Enum.map(repos, &(&1.id)) end)
     end
   end
 
   test "gets multiple repositories from multiple users", %{repos: repos} do
     users = Enum.map(repos, &(&1.owner))
-    assert Enum.all?(RepoQuery.user_repos(Enum.map(users, &(&1.id)), preload: :maintainers), fn repo -> repo.id in Enum.map(repos, &(&1.id)) end)
-    assert Enum.all?(RepoQuery.user_repos(Enum.map(users, &(&1.login)), preload: :maintainers), fn repo -> repo.id in Enum.map(repos, &(&1.id)) end)
-    assert Enum.all?(RepoQuery.user_repos(users, preload: :maintainers), fn repo -> repo.id in Enum.map(repos, &(&1.id)) end)
+    assert Enum.all?(RepoQuery.user_repos(Enum.map(users, &(&1.id))), fn repo -> repo.id in Enum.map(repos, &(&1.id)) end)
+    assert Enum.all?(RepoQuery.user_repos(Enum.map(users, &(&1.login))), fn repo -> repo.id in Enum.map(repos, &(&1.id)) end)
+    assert Enum.all?(RepoQuery.user_repos(users), fn repo -> repo.id in Enum.map(repos, &(&1.id)) end)
   end
 
   test "gets single repository by path", %{repos: repos} do
--- apps/gitgud/test/gitgud/user_query_test.exs
+++ apps/gitgud/test/gitgud/user_query_test.exs
@@ -10,32 +10,32 @@ defmodule GitGud.UserQueryTest do
 
   test "gets single user by id", %{users: users} do
     for user <- users do
-      assert user.id == UserQuery.by_id(user.id, preload: [:auth, :emails]).id
+      assert user.id == UserQuery.by_id(user.id).id
     end
   end
 
   test "gets multiple users by id", %{users: users} do
-    assert Enum.all?(UserQuery.by_id(Enum.map(users, &(&1.id)), preload: [:auth, :emails]), &(&1.id in Enum.map(users, fn user -> user.id end)))
+    assert Enum.all?(UserQuery.by_id(Enum.map(users, &(&1.id))), &(&1.id in Enum.map(users, fn user -> user.id end)))
   end
 
   test "gets single user by login", %{users: users} do
     for user <- users do
-      assert user.id == UserQuery.by_login(user.login, preload: [:auth, :emails]).id
+      assert user.id == UserQuery.by_login(user.login).id
     end
   end
 
   test "gets multiple users by login", %{users: users} do
-    assert Enum.all?(UserQuery.by_login(Enum.map(users, &(&1.login)), preload: [:auth, :emails]), &(&1.id in Enum.map(users, fn user -> user.id end)))
+    assert Enum.all?(UserQuery.by_login(Enum.map(users, &(&1.login))), &(&1.id in Enum.map(users, fn user -> user.id end)))
   end
 
   test "gets single user by email", %{users: users} do
     for user <- users do
-      assert user.id == UserQuery.by_email(hd(user.emails).address, preload: [:auth, :emails]).id
+      assert user.id == UserQuery.by_email(hd(user.emails).address).id
     end
   end
 
   test "gets multiple users by email", %{users: users} do
-    assert Enum.all?(UserQuery.by_email(Enum.map(users, &(hd(&1.emails).address)), preload: [:auth, :emails]), &(&1.id in  Enum.map(users, fn user -> user.id end)))
+    assert Enum.all?(UserQuery.by_email(Enum.map(users, &(hd(&1.emails).address))), &(&1.id in  Enum.map(users, fn user -> user.id end)))
   end
 
   #
