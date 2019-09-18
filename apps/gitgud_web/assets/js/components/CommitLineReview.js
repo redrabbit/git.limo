redrabbit/git.limo

commit dd7a5ac775f4da0f0317e7ff3b2c871565cf731c

Don't destroy <CommitLineReview /> with open form

apps/gitgud_web/assets/js/components/CommitLineReview.js
@@ -383,7 +383,7 @@ class CommitLineReview extends React.Component {
   }
 
   destroyComponent() {
-    if(this.state.comments.length === 0) {
+    if(this.state.folded && this.state.comments.length === 0) {
       let node = ReactDOM.findDOMNode(this)
       let container = node.closest(".inline-comments")
       ReactDOM.unmountComponentAtNode(node.parentNode)

commit 89720d7d524890198922f9ed8ec8bad5baf47990

Fix bug introduced in dd7a5ac

apps/gitgud_web/assets/js/components/CommitLineReview.js
@@ -50,6 +50,7 @@ class CommitLineReview extends React.Component {
   }
 
   componentWillUnmount() {
+    this.state.channel.leave()
     this.subscriptions.forEach(subscription => subscription.dispose())
   }
 
@@ -354,16 +355,16 @@ class CommitLineReview extends React.Component {
   }
 
   handleFormCancel() {
-    if(!this.destroyComponent()) {
-      this.setState({folded: true})
-    }
+    this.setState({folded: true}, this.destroyComponent)
   }
 
   handleFormTyping(isTyping) {
-    if(isTyping) {
-      this.state.channel.push("start_typing", {})
-    } else {
-      this.state.channel.push("stop_typing", {})
+    if(this.state.channel) {
+      if(isTyping) {
+        this.state.channel.push("start_typing", {})
+      } else {
+        this.state.channel.push("stop_typing", {})
+      }
     }
   }
 

apps/gitgud_web/assets/js/components/CommitReview.js
@@ -46,6 +46,7 @@ class CommitReview extends React.Component {
   }
 
   componentWillUnmount() {
+    this.channel.leave()
     this.subscriptions.forEach(subscription => subscription.dispose())
   }
 

apps/gitgud_web/assets/js/components/Issue.js
@@ -63,6 +63,7 @@ class Issue extends React.Component {
   }
 
   componentWillUnmount() {
+    this.channel.leave()
     this.subscriptions.forEach(subscription => subscription.dispose())
   }
 