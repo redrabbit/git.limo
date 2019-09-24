c4f214effdd2e5b2815605439e439ab63f170da1
Mario Flach <m.flach@almightycouch.com>
2019-09-24 22:17:40 +0200

    Add query batching for Markdown references
    Instead of fetching each issue reference and user mention separatly,
    we batch them together in a first regex scan.

--- apps/gitgud_web/lib/gitgud_web/views/helpers/markdown.ex
+++ apps/gitgud_web/lib/gitgud_web/views/helpers/markdown.ex
@@ -19,6 +19,8 @@ defmodule GitGud.Web.Markdown do
   def markdown(content, opts \\ [])
   def markdown(nil, _opts), do: nil
   def markdown(content, opts) do
+    opts = Keyword.put_new_lazy(opts, :users, fn -> find_user_mentions(content) end)
+    opts = Keyword.put_new_lazy(opts, :issues, fn -> find_issue_references(content, Keyword.get(opts, :repo)) end)
     case Earmark.as_ast(content) do
       {:ok, ast, _warnings} ->
         ast
@@ -53,13 +55,15 @@ defmodule GitGud.Web.Markdown do
   defp transform_ast_node(content, opts) when is_binary(content) do
     transform_ast_node_text(
       content,
-      Regex.scan(~r/\B(#[0-9]+)\b|\B(@[a-zA-Z0-9_-]+)\b|:([a-z0-1\+]+):|\b([a-f0-9]{7})\b/, content, capture: :first, return: :index),
-      Keyword.get(opts, :repo)
+      Regex.scan(~r/\B(@[a-zA-Z0-9_-]+)\b|\B(#[0-9]+)\b|:([a-z0-1\+]+):|\b([a-f0-9]{7})\b/, content, capture: :first, return: :index),
+      Keyword.get(opts, :repo),
+      Keyword.get(opts, :users),
+      Keyword.get(opts, :issues)
     )
   end
 
-  defp transform_ast_node_text(content, [], _repo), do: content
-  defp transform_ast_node_text(content, indexes, repo) do
+  defp transform_ast_node_text(content, [], _repo, _users, _issues), do: content
+  defp transform_ast_node_text(content, indexes, repo, users, issues) do
     {content, rest, _offset} =
       Enum.reduce(List.flatten(indexes), {[], content, 0}, fn {idx, len}, {acc, rest, offset} ->
         ofs = idx-offset
@@ -67,12 +71,13 @@ defmodule GitGud.Web.Markdown do
         <<match::binary-size(len), rest::binary>> = rest
         link =
           case match do
-            "#" <> number ->
-              if issue = repo && IssueQuery.repo_issue(repo, String.to_integer(number)), do:
-                {"a", [{"href", Routes.issue_path(GitGud.Web.Endpoint, :show, repo.owner, repo, issue)}], ["##{number}"]}
             "@" <> login ->
-              if user = UserQuery.by_login(login), do:
+              if user = Enum.find(users, &(&1.login == login)), do:
                 {"a", [{"href", Routes.user_path(GitGud.Web.Endpoint, :show, user)}, {"class", "has-text-black"}], ["@#{login}"]}
+            "#" <> number_str ->
+              number = String.to_integer(number_str)
+              if issue = Enum.find(issues, &(&1.number == number)), do:
+                {"a", [{"href", Routes.issue_path(GitGud.Web.Endpoint, :show, repo.owner, repo, issue)}], ["##{number}"]}
             match ->
               cond do
                 String.starts_with?(match, ":") && String.ends_with?(match, ":") ->
@@ -99,4 +104,28 @@ defmodule GitGud.Web.Markdown do
    else: match
   end
 
+
+  defp find_issue_references(content, nil), do: []
+  defp find_issue_references(content, repo) do
+    numbers =
+      ~r/\B#([0-9]+)\b/
+      |> Regex.scan(content, capture: :all_but_first)
+      |> List.flatten()
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.uniq()
+    unless Enum.empty?(numbers),
+      do: IssueQuery.repo_issues(repo, numbers: numbers),
+    else: []
+  end
+
+  defp find_user_mentions(content) do
+    logins =
+      ~r/\B@([a-zA-Z0-9_-]+)\b/
+      |> Regex.scan(content, capture: :all_but_first)
+      |> List.flatten()
+      |> Enum.uniq()
+    unless Enum.empty?(logins),
+      do: UserQuery.by_login(logins),
+    else: []
+  end
 end
