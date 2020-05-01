926c5a056f9a884c73834d764a44615addb5b027
Benjamin Scholtz <contact@benjaminscholtz.com>
2020-05-01 15:43:55 +0200

    Add Nix shell

---
+++ default.nix
@@ -0,0 +1,21 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+with pkgs;
+
+let
+  inherit (lib) optional optionals;
+
+  elixir = beam.packages.erlangR22.elixir_1_10;
+  nodejs = nodejs-13_x;
+  postgresql = postgresql_12;
+in
+
+mkShell {
+  buildInputs = [ elixir nodejs git postgresql cmake libgit2 gcc zlib python38 ]
+    ++ optional stdenv.isLinux inotify-tools # For file_system on Linux.
+    ++ optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
+      # For file_system on macOS.
+      CoreFoundation
+      CoreServices
+    ]);
+}
