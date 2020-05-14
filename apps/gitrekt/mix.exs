ad7e9b97bbab881d7259beffeaf2ae698ab0c62f
Mario Flach <m.flach@almightycouch.com>
2020-05-14 22:51:00 +0200

    Bump version to 0.3.1

--- apps/gitrekt/mix.exs
+++ apps/gitrekt/mix.exs
@@ -3,7 +3,7 @@ defmodule GitRekt.Mixfile do
 
   def project do
     [app: :gitrekt,
-     version: "0.3.0",
+     version: "0.3.1",
      build_path: "../../_build",
      config_path: "../../config/config.exs",
      deps_path: "../../deps",
