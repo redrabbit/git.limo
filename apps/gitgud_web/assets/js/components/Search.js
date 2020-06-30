34cdb47824803b640a5f5dd73562b63959ac3d1c
Mario Flach <m.flach@almightycouch.com>
2020-06-30 22:31:17 +0200

    Allow arrow navigation in <Search />

--- apps/gitgud_web/assets/js/components/Search.js
+++ apps/gitgud_web/assets/js/components/Search.js
@@ -12,9 +12,10 @@ class Search extends React.Component {
     super(props)
     this.dropdown = React.createRef()
     this.inputContainer = React.createRef()
+    this.handleInputKeyDown = this.handleInputKeyDown.bind(this)
     this.handleInputChange = this.handleInputChange.bind(this)
     this.renderSearchResult = this.renderSearchResult.bind(this)
-    this.state = {input: "", results: []}
+    this.state = {input: "", results: [], activeItem: 0}
   }
 
   componentDidUpdate(prevProps, prevState) {
@@ -50,9 +51,7 @@ class Search extends React.Component {
       }
 
       fetchQuery(environment, query, variables)
-        .then(response => {
-          this.setState({results: response.search.edges.map(edge => edge.node)})
-        })
+        .then(response => this.setState({results: response.search.edges.map(edge => edge.node), activeItem: 0}))
     }
   }
 
@@ -60,7 +59,7 @@ class Search extends React.Component {
     return (
       <div className="dropdown" ref={this.dropdown}>
         <div className="control has-icons-left">
-          <input type="text" className="input" ref={this.inputContainer} onChange={this.handleInputChange} placeholder="Search ..." />
+          <input type="text" className="input" ref={this.inputContainer} onKeyDown={this.handleInputKeyDown} onChange={this.handleInputChange} placeholder="Search ..." />
           <span className="icon is-small is-left">
             <i className="fa fa-search"></i>
           </span>
@@ -73,18 +72,31 @@ class Search extends React.Component {
   }
 
   renderDropdown() {
-    return (
-      <div className="dropdown-content">
-        {this.state.results.map(this.renderSearchResult)}
-      </div>
-    )
+    if(this.state.results.length > 0) {
+      return (
+        <div className="dropdown-content">
+          {this.state.results.map(this.renderSearchResult)}
+        </div>
+      )
+    } else {
+      return (
+        <div className="dropdown-content">
+          <div className="dropdown-item">
+            Nothing to see here.
+          </div>
+        </div>
+      )
+    }
   }
 
-  renderSearchResult(edge) {
+  renderSearchResult(edge, i) {
+    let classNames = ["dropdown-item"]
+    if(i == this.state.activeItem)
+      classNames.push("is-selected")
     switch(edge.__typename) {
       case "User":
         return (
-          <a key={edge.id} href={edge.url} className="dropdown-item">
+          <a key={edge.id} href={edge.url} className={classNames.join(" ")} onMouseOver={() => this.setState({activeItem: i})}>
             <span className="tag user is-white">
               <img className="avatar is-small" src={edge.avatarUrl} width={24} />{edge.login}
             </span>
@@ -92,7 +104,7 @@ class Search extends React.Component {
         )
       case "Repo":
         return (
-          <a key={edge.id} href={edge.url} className="dropdown-item">
+          <a key={edge.id} href={edge.url} className={classNames.join(" ")} onMouseOver={() => this.setState({activeItem: i})}>
             <div className="tags has-addons">
               <span className="tag user is-white">
                 <img className="avatar is-small" src={edge.owner.avatarUrl} width={24} />{edge.owner.login}
@@ -101,6 +113,28 @@ class Search extends React.Component {
             </div>
           </a>
         )
+    }
+  }
+
+  handleInputKeyDown(event) {
+    if(this.state.results.length > 0) {
+      switch(event.key) {
+        case "Enter":
+          window.location.href = this.state.results[this.state.activeItem].url
+          break
+        case "ArrowUp":
+          this.setState(state => {
+            return {activeItem: state.activeItem == 0 ? state.results.length - 1 : state.activeItem  - 1}
+          })
+          event.preventDefault()
+          break
+        case "ArrowDown":
+          this.setState(state => {
+            return {activeItem: state.activeItem < state.results.length - 1 ? state.activeItem  + 1 : 0}
+          })
+          event.preventDefault()
+          break
+      }
     }
   }
 
