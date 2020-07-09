b69584098e952e92287f22e9d3a5d509d29041e6
Mario Flach <m.flach@almightycouch.com>
2020-07-09 23:27:41 +0200

    Add unique index to repositories contributors

--- apps/gitgud/priv/db/migrations/20171009222935_add_repositories_table.exs
+++ apps/gitgud/priv/db/migrations/20171009222935_add_repositories_table.exs
@@ -17,5 +17,7 @@ defmodule GitGud.DB.Migrations.AddRepositoriesTable do
       add :repo_id, references("repositories", on_delete: :delete_all), null: false
       add :user_id, references("users", on_delete: :delete_all), null: false
     end
+
+    create unique_index("repositories_contributors", [:repo_id, :user_id])
   end
 end
