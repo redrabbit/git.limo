4cf95cebd218965f70e4890fe03da339e4cd1dc6
Mario Flach <m.flach@almightycouch.com>
2021-03-13 00:27:12 +0100

    Add GitGud.Web.MaintainerSearchFormLive
    This new LiveView replaces GitGud.Web.MaintainerSearchLive by
    implementing the <form> rendering and submission.
    
    It still submit the <form> to GitGud.Web.MaintainerController.create/2
    but perform validation on it's own.

---
+++ apps/gitgud_web/lib/gitgud_web/live/maintainer_search_form_live.ex
@@ -0,0 +1,75 @@
+defmodule GitGud.Web.MaintainerSearchFormLive do
+  use GitGud.Web, :live_view
+
+  alias GitGud.UserQuery
+  alias GitGud.RepoQuery
+
+  import Ecto.Changeset
+
+  #
+  # Callbacks
+  #
+
+  @impl true
+  def mount(_params, %{"repo_id" => repo_id} = session, socket) do
+    {
+      :ok,
+      socket
+      |> authenticate(session)
+      |> assign_new(:repo, fn -> RepoQuery.by_id(repo_id, preload: :maintainers) end)
+      |> assign_new(:changeset, &changeset/0)
+      |> assign(active: false, search_results: [], trigger_submit: false)
+    }
+  end
+
+  @impl true
+  def handle_event("search", %{"maintainer" => maintainer_params}, socket) do
+    changeset = changeset(maintainer_params, socket.assigns.search_results, socket.assigns.repo.maintainers)
+    if search = get_change(changeset, :user_login) do
+      search_results = UserQuery.search(search, viewer: socket.assigns.current_user)
+      search_results = Enum.reject(search_results, &(&1 in socket.assigns.repo.maintainers))
+      {:noreply, assign(socket, active: search != "", changeset: changeset, search_results: search_results)}
+    else
+      {:noreply, assign(socket, active: false, changeset: changeset, search_results: [])}
+    end
+  end
+
+  def handle_event("submit", %{"maintainer" => maintainer_params}, socket), do: submit_form(socket, maintainer_params)
+  def handle_event("select", maintainer_params, socket), do: submit_form(socket, maintainer_params)
+
+  #
+  # Helpers
+  #
+
+  defp changeset(params \\ %{}, available_users \\ [], rejected_users \\ []) do
+    types = %{user_login: :string}
+    {%{}, types}
+    |> cast(params, Map.keys(types))
+    |> validate_required([:user_login])
+    |> validate_login(available_users, rejected_users)
+  end
+
+  defp validate_login(changeset, available_users, rejected_users) do
+    user_login = get_change(changeset, :user_login)
+    cond do
+      is_nil(user_login) ->
+        changeset
+      Enum.find(available_users, &(&1.login == user_login)) ->
+        changeset
+      Enum.find(rejected_users, &(&1.login == user_login)) ->
+        add_error(changeset, :user_login, "has already been taken")
+      true ->
+        add_error(changeset, :user_login, "invalid")
+    end
+  end
+
+  defp submit_form(socket, maintainer_params) do
+    changeset = changeset(maintainer_params, socket.assigns.search_results, socket.assigns.repo.maintainers)
+    case apply_action(changeset, :insert) do
+      {:ok, _maintainer} ->
+        {:noreply, assign(socket, active: false, changeset: changeset, trigger_submit: true)}
+      {:error, changeset} ->
+        {:noreply, assign(socket, active: false, changeset: changeset)}
+    end
+  end
+end
