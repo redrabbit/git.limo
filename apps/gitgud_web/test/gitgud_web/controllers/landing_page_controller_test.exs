79b94d9dce885efbaf84c69d708e29fea9f9aad4
Mario Flach <m.flach@almightycouch.com>
2021-04-27 23:50:56 +0200

    Fix GitGud.Web.LandingPageControllerTest

--- apps/gitgud_web/test/gitgud_web/controllers/landing_page_controller_test.exs
+++ apps/gitgud_web/test/gitgud_web/controllers/landing_page_controller_test.exs
@@ -1,8 +1,11 @@
 defmodule GitGud.Web.LandingPageControllerTest do
   use GitGud.Web.ConnCase, async: true
 
+  alias GitGud.Web.LayoutView
+
   test "renders landing page", %{conn: conn} do
     conn = get(conn, Routes.landing_page_path(conn, :index))
-    assert html_response(conn, 200) =~ ~s(<title>Code collaboration for dev teams</title>)
+    assert {:ok, html} = Floki.parse_document(html_response(conn, 200))
+    assert Floki.text(Floki.find(html, "title")) == LayoutView.title(conn)
   end
 end
