b0e20c4b2cfbd705eabc0a1853c3fb92f3e26716
Mario Flach <m.flach@almightycouch.com>
2021-04-27 23:30:29 +0200

    Fix GitGud.Web.GPGKeyControllerTest

--- apps/gitgud_web/test/gitgud_web/controllers/gpg_key_controller_test.exs
+++ apps/gitgud_web/test/gitgud_web/controllers/gpg_key_controller_test.exs
@@ -6,6 +6,8 @@ defmodule GitGud.Web.GPGKeyControllerTest do
   alias GitGud.User
   alias GitGud.GPGKey
 
+  alias GitGud.Web.LayoutView
+
   import GitGud.Web.GPGKeyView
 
   setup :create_user
@@ -13,12 +15,14 @@ defmodule GitGud.Web.GPGKeyControllerTest do
   test "renders gpg authentication key creation form if authenticated", %{conn: conn, user: user} do
     conn = Plug.Test.init_test_session(conn, user_id: user.id)
     conn = get(conn, Routes.gpg_key_path(conn, :new))
-    assert html_response(conn, 200) =~ ~s(<h2 class="subtitle">Add a new GPG key</h2>)
+    assert {:ok, html} = Floki.parse_document(html_response(conn, 200))
+    assert Floki.text(Floki.find(html, "title")) == LayoutView.title(conn)
   end
 
   test "fails to render gpg authentication key creation form if not authenticated", %{conn: conn} do
     conn = get(conn, Routes.gpg_key_path(conn, :new))
-    assert html_response(conn, 401) =~ "Unauthorized"
+    assert {:ok, html} = Floki.parse_document(html_response(conn, 401))
+    assert Floki.text(Floki.find(html, "title")) == "Oops, something went wrong!"
   end
 
   @tag :skip
@@ -36,7 +40,8 @@ defmodule GitGud.Web.GPGKeyControllerTest do
     conn = Plug.Test.init_test_session(conn, user_id: user.id)
     conn = post(conn, Routes.gpg_key_path(conn, :create), gpg_key: Map.put(gpg_key_params, :data, "-----BEGIN PGP PUBLIC KEY BLOCK-----\n-----END PGP PUBLIC KEY BLOCK-----\n"))
     assert get_flash(conn, :error) == "Something went wrong! Please check error(s) below."
-    assert html_response(conn, 400) =~ ~s(<h2 class="subtitle">Add a new GPG key</h2>)
+    assert {:ok, html} = Floki.parse_document(html_response(conn, 400))
+    assert Floki.text(Floki.find(html, "title")) == LayoutView.title(conn)
   end
 
   describe "when gpg authentication keys exist" do
@@ -46,9 +51,11 @@ defmodule GitGud.Web.GPGKeyControllerTest do
     test "renders user gpg authentication keys", %{conn: conn, user: user, gpg_keys: gpg_keys} do
       conn = Plug.Test.init_test_session(conn, user_id: user.id)
       conn = get(conn, Routes.gpg_key_path(conn, :index))
-      assert html_response(conn, 200) =~ ~s(<h2 class="subtitle">GPG keys</h2>)
+      assert {:ok, html} = Floki.parse_document(html_response(conn, 200))
+      assert Floki.text(Floki.find(html, "title")) == LayoutView.title(conn)
+      html_gpg_keys = Enum.map(Floki.find(html, ".card .card-header-title"), &String.trim(Floki.text(&1)))
       for gpg_key <- gpg_keys do
-        assert html_response(conn, 200) =~ ~s(<code class=\"is-size-7\">0x#{format_key_id(gpg_key.key_id)}</code>)
+        assert "0x" <> format_key_id(gpg_key.key_id) in html_gpg_keys
       end
     end
 
