1d230ec3bbf22a99976cc0905dfb4a1eb4a8ccff
Mario Flach <m.flach@almightycouch.com>
2021-10-31 00:59:20 +0200

    Add progress bar animation support with topbar.js

--- apps/gitgud_web/assets/js/hooks/live-views.js
+++ apps/gitgud_web/assets/js/hooks/live-views.js
@@ -1,8 +1,22 @@
+import topbar from "topbar"
+
 import liveSocket from "../live-socket"
 
 export default () => {
   const liveViews = document.querySelectorAll("[data-phx-session]:not([data-phx-connect-later])")
   if(liveViews.length) {
+    topbar.config({
+      barThickness: 1,
+      barColors: {
+        0: "#2ec2a7",
+        1: "#f3d270"
+      },
+      shadowColor: "rgba(0, 0, 0, .3)"
+    })
+
+    window.addEventListener("phx:page-loading-start", info => topbar.show())
+    window.addEventListener("phx:page-loading-stop", info => topbar.hide())
+
     liveSocket.connect()
   }
 }
