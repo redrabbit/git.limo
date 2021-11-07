4d20b1b2e25b387da010656dceedb6c1d50a7378
Mario Flach <m.flach@almightycouch.com>
2021-11-08 00:21:36 +0100

    Add presence support for GitGud.Web.IssueLive

--- apps/gitgud_web/lib/gitgud_web/live/issue_live.ex
+++ apps/gitgud_web/lib/gitgud_web/live/issue_live.ex
@@ -15,6 +15,8 @@ defmodule GitGud.Web.IssueLive do
   alias GitGud.IssueQuery
   alias GitGud.CommentQuery
 
+  alias GitGud.Web.Presence
+
   import GitGud.Web.Endpoint, only: [broadcast_from: 4, subscribe: 1]
 
   #
@@ -33,8 +35,10 @@ defmodule GitGud.Web.IssueLive do
       |> assign_issue(String.to_integer(number))
       |> assign_page_title()
       |> assign_title_changeset()
-      |> assign(:title_edit, false)
-      |> subscribe_topic(),
+      |> assign_presence!()
+      |> assign_presence_map()
+      |> assign_users_typing()
+      |> subscribe_topic!(),
       temporary_assigns: [issue_feed: []]
     }
   end
@@ -46,7 +50,7 @@ defmodule GitGud.Web.IssueLive do
       {:ok, data} ->
         issue = Issue.update_title!(socket.assigns.issue, data.title, user_id: current_user(socket).id)
         event = List.last(issue.events)
-        broadcast_from(self(), "issue:#{socket.assigns.issue.id}", "update_title", %{title: issue.title, event: event})
+        broadcast_from(self(), issue_topic(socket), "update_title", %{title: issue.title, event: event})
         {
           :noreply,
           socket
@@ -77,29 +81,31 @@ defmodule GitGud.Web.IssueLive do
     case Issue.add_comment(socket.assigns.issue, current_user(socket), comment_params["body"]) do
       {:ok, comment} ->
         send_update(GitGud.Web.CommentFormLive, id: "issue-comment-form", changeset: Comment.changeset(%Comment{}))
-        broadcast_from(self(), "issue:#{socket.assigns.issue.id}", "add_comment", %{comment_id: comment.id})
+        broadcast_from(self(), issue_topic(socket), "add_comment", %{comment_id: comment.id})
         {
           :noreply,
           socket
           |> assign(:issue_comment_count, socket.assigns.issue_comment_count + 1)
           |> assign(:issue_feed, [comment])
+          |> assign_presence_typing!(false)
           |> push_event("add_comment", %{comment_id: comment.id})
         }
       {:error, changeset} ->
         send_update(GitGud.Web.CommentFormLive, id: "issue-comment-form", changeset: changeset)
-        {:noreply, socket}
+        {:noreply, assign_presence_typing!(socket, false)}
     end
   end
 
   def handle_event("validate_comment", %{"comment" => comment_params}, socket) do
-    send_update(GitGud.Web.CommentFormLive, id: "issue-comment-form", changeset: Comment.changeset(%Comment{}, comment_params))
-    {:noreply, socket}
+    changeset = Comment.changeset(%Comment{}, comment_params)
+    send_update(GitGud.Web.CommentFormLive, id: "issue-comment-form", changeset: changeset)
+    {:noreply, assign_presence_typing!(socket, !!Ecto.Changeset.get_change(changeset, :body))}
   end
 
   def handle_event("close", _params, socket) do
     issue = Issue.close!(socket.assigns.issue, user_id: current_user(socket).id)
     event = List.last(issue.events)
-    broadcast_from(self(), "issue:#{socket.assigns.issue.id}", "close", %{event: event})
+    broadcast_from(self(), issue_topic(socket), "close", %{event: event})
     {
       :noreply,
       socket
@@ -111,13 +117,12 @@ defmodule GitGud.Web.IssueLive do
   def handle_event("reopen", _params, socket) do
     issue = Issue.reopen!(socket.assigns.issue, user_id: current_user(socket).id)
     event = List.last(issue.events)
-    broadcast_from(self(), "issue:#{socket.assigns.issue.id}", "reopen", %{event: event})
+    broadcast_from(self(), issue_topic(socket), "reopen", %{event: event})
     {
       :noreply,
       socket
       |> assign(:issue, issue)
       |> assign(:issue_feed, [{Map.put(event, "user", current_user(socket)), length(issue.events)}])
-      |> push_event("add_event", %{event_id: length(issue.events)})
     }
   end
 
@@ -125,7 +130,7 @@ defmodule GitGud.Web.IssueLive do
   def handle_info({:update_labels, {push_ids, pull_ids} = changes}, socket) do
     issue = Issue.update_labels!(socket.assigns.issue, changes, user_id: current_user(socket).id)
     event = List.last(issue.events)
-    broadcast_from(self(), "issue:#{socket.assigns.issue.id}", "update_labels", %{push_ids: push_ids, pull_ids: pull_ids, event: event})
+    broadcast_from(self(), issue_topic(socket), "update_labels", %{push_ids: push_ids, pull_ids: pull_ids, event: event})
     {
       :noreply,
       socket
@@ -135,12 +140,12 @@ defmodule GitGud.Web.IssueLive do
   end
 
   def handle_info({:update_comment, comment_id}, socket) do
-    broadcast_from(self(), "issue:#{socket.assigns.issue.id}", "update_comment", %{comment_id: comment_id})
+    broadcast_from(self(), issue_topic(socket), "update_comment", %{comment_id: comment_id})
     {:noreply, push_event(socket, "update_comment", %{comment_id: comment_id})}
   end
 
   def handle_info({:delete_comment, comment_id}, socket) do
-    broadcast_from(self(), "issue:#{socket.assigns.issue.id}", "delete_comment", %{comment_id: comment_id})
+    broadcast_from(self(), issue_topic(socket), "delete_comment", %{comment_id: comment_id})
     {
       :noreply,
       socket
@@ -234,16 +239,18 @@ defmodule GitGud.Web.IssueLive do
     }
   end
 
+  def handle_info(%Phoenix.Socket.Broadcast{event: "presence_diff", payload: %{joins: joins, leaves: leaves}}, socket) do
+    {
+      :noreply,
+      socket
+      |> assign_presence_map(joins, leaves)
+      |> assign_users_typing()
+    }
+  end
+
   #
   # Helpers
   #
-
-  defp subscribe_topic(socket) do
-    if connected?(socket) do
-      subscribe("issue:#{socket.assigns.issue.id}")
-    end
-    socket
-  end
 
   defp assign_repo!(socket, user_login, repo_name) do
     query = DBQueryable.query({RepoQuery, :user_repo_query}, [user_login, repo_name], viewer: current_user(socket), preload: :issue_labels)
@@ -286,9 +293,40 @@ defmodule GitGud.Web.IssueLive do
   end
 
   defp assign_title_changeset(socket) do
-    if connected?(socket),
-      do: assign(socket, :title_changeset, title_changeset(socket.assigns.issue)),
-    else: socket
+    assign(socket, title_changeset: title_changeset(socket.assigns.issue), title_edit: false)
+  end
+
+  defp assign_presence!(socket) do
+    if connected?(socket) && authenticated?(socket) do
+      {:ok, presence_ref} = Presence.track(self(), issue_topic(socket), current_user(socket).login, %{typing: false})
+      assign(socket, presence_ref: presence_ref, presence_typing: false)
+    else
+      assign(socket, presence_ref: nil, presence_typing: false)
+    end
+  end
+
+  defp assign_presence_map(socket) do
+    assign(socket, :presence_map, Presence.list(issue_topic(socket)))
+  end
+
+  defp assign_presence_map(socket, joins, leaves) do
+    presences = socket.assigns.presence_map
+    presences = Enum.reduce(leaves, presences, fn {key, _presence}, acc -> Map.delete(acc, key) end)
+    presences = Enum.reduce(joins, presences, fn {key, presence}, acc -> Map.put(acc, key, presence) end)
+    assign(socket, :presence_map, presences)
+  end
+
+  defp assign_presence_typing!(socket, presence_typing) do
+    if socket.assigns.presence_typing != presence_typing do
+      {:ok, presence_ref} = Presence.update(self(), issue_topic(socket), current_user(socket).login, &Map.put(&1, :typing, presence_typing))
+      assign(socket, presence_ref: presence_ref, presence_typing: presence_typing)
+    else
+      socket
+    end
+  end
+
+  defp assign_users_typing(socket) do
+    assign(socket, :users_typing, Enum.flat_map(socket.assigns.presence_map, &filter_map_presence_to_user_typing(&1, socket.assigns.presence_ref)))
   end
 
   defp resolve_feed(issue) do
@@ -310,4 +348,19 @@ defmodule GitGud.Web.IssueLive do
     |> Ecto.Changeset.cast(params, Map.keys(types))
     |> Ecto.Changeset.validate_required([:title])
   end
+
+  defp filter_map_presence_to_user_typing({user_login, presence}, presence_ref) do
+    if Enum.any?(presence.metas, &(&1.typing && &1.phx_ref != presence_ref)),
+      do: [user_login],
+    else: []
+  end
+
+  defp subscribe_topic!(socket) do
+    if connected?(socket) do
+      :ok = subscribe(issue_topic(socket))
+    end
+    socket
+  end
+
+  defp issue_topic(socket), do: "issue:#{socket.assigns.issue.id}"
 end
